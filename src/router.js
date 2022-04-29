b1ed8ae2a2c12d571d1833273e87fa232edea86a
issei_ezawa <issemann@yahoo.co.jp>
2022-04-29 13:02:52 +0900

    Add: vue-routerの初期設定追加

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Top from './components/TopPage'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Top
+    }
+  ]
+})
