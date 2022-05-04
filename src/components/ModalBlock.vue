eabdaab277c505709b0afba4e3970fa27afda83c
issei_ezawa <issemann@yahoo.co.jp>
2022-05-04 13:52:58 +0900

    Add: animejsを適用させたモーダルの作成

--- src/components/ModalBlock.vue
+++ src/components/ModalBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="modal">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" @click="closeModal(amount)">
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'modal-block',
   props: ['amount'],
